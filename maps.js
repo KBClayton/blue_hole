ee1caa3c6b06f6e39314fb4980f9eaa3e3778c4b
KBClayton <clayton.kevinb@gmail.com>
2018-07-05 11:40:05 -0500

    stuff

---
+++ maps.js
@@ -0,0 +1,108 @@
+
+$(document).ready(function() {
+
+
+
+
+    var location_001={lat: 30.003420, lng:-98.091782}
+
+    var point_001 = new google.maps.Marker({
+        position: location_001,
+        title: "Creek bed at blue hole park"
+    });
+    point_001.setMap(map); 
+        
+    
+//     var location_002={lat: 32.637105, lng:-98.093025}
+
+//     var point_002 = new google.maps.Marker({
+//         position: location_002,
+//         title: "Diversion point 61204013002"
+//     });
+//     point_002.setMap(map); 
+    
+//     var location_003={lat: 32.655152, lng:-98.083317}
+
+//     var point_003 = new google.maps.Marker({
+//         position: location_003,
+//         title: "Diversion point 61204013003"
+//     });
+//     point_003.setMap(map); 
+    
+//     var location_004={lat: 32.677391, lng:-98.064417}
+
+//     var point_004 = new google.maps.Marker({
+//         position: location_004,
+//         title: "Diversion point 61204013004"
+//     });
+//     point_004.setMap(map); 
+    
+//     var location_005={lat: 32.684237, lng:-98.073618}
+
+//     var point_005 = new google.maps.Marker({
+//         position: location_005,
+//         title: "Diversion point 61204013011 & On-channel Reservoir 61204013307"
+//     });
+//     point_005.setMap(map); 
+    
+//     var location_006={lat: 32.671549, lng:-98.083757}
+
+//     var point_006 = new google.maps.Marker({
+//         position: location_006,
+//         title: "Diversion point 61204013010 & On-channel Reservoir 61204013306"
+//     });
+//     point_006.setMap(map); 
+    
+//     var location_007={lat: 32.663729, lng:-98.096344}
+
+//     var point_007 = new google.maps.Marker({
+//         position: location_007,
+//         title: "Diversion point 61204013006 & On-channel Reservoir 61204013302"
+//     });
+//     point_007.setMap(map); 
+    
+//     var location_008={lat: 32.663216, lng:-98.094527}
+
+//     var point_008 = new google.maps.Marker({
+//         position: location_008,
+//         title: "Diversion point 61204013005 & On-channel Reservoir 61204013301"
+//     });
+//     point_008.setMap(map); 
+
+    
+//     var location_009={lat: 32.659454, lng:-98.094627}
+
+//     var point_009 = new google.maps.Marker({
+//         position: location_009,
+//         title: "Diversion point 61204013008 & On-channel Reservoir 61204013304"
+//     });
+//     point_009.setMap(map); 
+
+    
+//     var location_010={lat: 32.659730, lng:-98.093472}
+
+//     var point_010 = new google.maps.Marker({
+//         position: location_010,
+//         title: "Diversion point 61204013007 & On-channel Reservoir 61204013303"
+//     });
+//     point_010.setMap(map); 
+
+
+//     var location_011={lat: 32.669692, lng:-98.068513}
+
+//     var point_011 = new google.maps.Marker({
+//         position: location_011,
+//         title: "Diversion point 61204013012"
+//     });
+//     point_011.setMap(map); 
+    
+//     var location_012={lat: 32.659010, lng:-98.081757}
+
+//     var point_012 = new google.maps.Marker({
+//         position: location_012,
+//         title: "Diversion point 61204013009 & On-channel Reservoir 61204013305"
+//     });
+//     point_012.setMap(map); 
+    
+
+});
